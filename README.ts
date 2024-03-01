5cecfd1d60df6903c4f8021b13509c3373d0c5b8
bhaskarreddy463 <bhaskarreddy.463@gmail.com>
2024-03-01 12:08:57 +0530

    Update README.ts

--- README.ts
+++ README.ts
@@ -252,3 +252,172 @@ export class GroupByComponent implements OnInit, OnChanges, OnDestroy, ControlVa
     </div>
   </div>
 </gs-menu>
+
+
+
+<div class="row mt-2 gx-3 aggregation-table__date-picker" *ngIf="showDatePicker">
+  <div class="col-auto py-2">
+    <metrics-date-picker [defaultDate]="defaultDate" [selectedDate]="selectedRiskDate" [renderAsLabel]="!canChooseDate"
+      [showLabel]="false" (dateChanged)="dateChanged($event)" (cleared)="clearDate()"></metrics-date-picker>
+  </div>
+</div>
+<div class="row mt-1 g-0" *ngIf="sections?.length">
+  <gs-tabs [classes]="{ container: overrideClasses.gsTabsSmall }" class="col" [activeId]="activeSection.categoryId">
+    <gs-tab *ngFor="let category of sections; trackBy: trackCategoryId" [id]="category.categoryId" [title]="category.categoryName"
+      (click)="switchSection(category)"></gs-tab>
+  </gs-tabs>
+</div>
+<div class="row mt-2 g-0 flex-nowrap aggregation-table__sub-sections" metricsDetectOverflow [reservedWidth]="100"
+  (overflowingElementIndex)="overflowingTabs($event)" *ngIf="titles.length && titles.length > 1">
+  <div *ngFor="let packet of titles; first as isFirst" class="col-auto me-1" [class.ms-1]="!isFirst"
+    [class.d-none]="!packetsToRender.includes(packet)">
+    <gs-button #button size="sm" shape="circle" class="metrics-button"
+      (click)="switchSection(activeSection, packet, button)" emphasis="subtle"
+      [actionType]="packet === activeSubSection ? 'primary' : 'secondary'">
+      {{ packet }}</gs-button>
+  </div>
+  <div *ngIf="overflowMenu.length" class="col-auto ms-1 aggregation-table__sub-sections-more-button">
+    <gs-dropdown size="sm" [menuVisible]="overflowDropdownVisible">
+      <gs-button #overflowButton size="sm" shape="circle" size="sm" emphasis="subtle" class="metrics-button"
+        [actionType]="overflowMenu.includes(activeSubSection) ? 'primary' : 'secondary'"
+        (click)="toggleOverflowMenu()">More
+        <gs-icon type="filled" role="img" size="md" [name]="overflowDropdownVisible ? 'expand-less' : 'expand-more'">
+        </gs-icon></gs-button>
+      <gs-dropdown-menu (change)="switchSectionViaMenu(activeSection, $event, overflowButton)" (blur)="blur($event)">
+        <gs-menu-option *ngFor="let item of overflowMenu" [value]="item">
+          <div class="row g-0 m-0"><span class="col-auto">{{ item }}</span>
+            <gs-icon class="ms-auto col-auto gs-blue060-link" size="sm" type="outlined" name="check"
+              *ngIf="item === activeSubSection"></gs-icon>
+          </div>
+        </gs-menu-option>
+      </gs-dropdown-menu>
+    </gs-dropdown>
+  </div>
+</div>
+<ng-container *ngIf="packetId === 'SFDR'">
+  <div class="d-flex pe-3 ps-1 pb-1 my-2 article8-metrics-wrapper">
+    <gs-metric size="xs" label="Article 8 Fund?" [value]="isArticle8Account || '-'">
+    </gs-metric>
+    <gs-metric size="xs"
+      [legendColor]="isArticle8Compliant === true ? 'green040' : isArticle8Compliant === false ? 'red060' : 'gray060'"
+      label="Article 8 Compliant?" [value]="isArticle8Compliant ? 'Yes' : isArticle8Compliant === false ? 'No' : '-'">
+    </gs-metric>
+  </div>
+</ng-container>
+<gs-table *ngIf="!error && columnConfig" cellAlignment="center" class="table w-100" size="sm">
+  <thead width="100%">
+    <tr>
+      <th align="left" width="33%">&nbsp;</th>
+      <ng-container *ngFor="let column of columnConfig; trackBy: trackColumn">
+        <th class="text-end" [style.width.%]="66 / columnConfig.length" *ngIf="!column.hideFrom360Page">
+          <ng-container *ngIf="column.displayName === 'Act'">
+            {{ selectedAccount.shortName ? selectedAccount.shortName + ' [' + selectedAccount.invest1Id + ']' : '&nbsp;'
+            }}
+          </ng-container>
+          <ng-container *ngIf="column.displayName !== 'Act'">
+            {{ column.displayName }}</ng-container>
+        </th>
+      </ng-container>
+    </tr>
+  </thead>
+  <tr class="aggregation-table__search">
+    <td align="left" [attr.colspan]="1 + columnConfig.length">
+      <gs-input size="sm" type="search" placeholder="Search" [inline]="true" [(ngModel)]="searchValue" (valueChange)="searchValueChanged($event)">
+      </gs-input>
+    </td>
+  </tr>
+</gs-table>
+<div class="flex-grow-1 aggregation-table position-relative">
+  <gs-loading-overlay [visible]="loading" class="col position-sticky">
+    <gs-loading-icon shape="circle" [label]="false"></gs-loading-icon>
+  </gs-loading-overlay>
+  <gs-table *ngIf="!error && columnConfig" cellAlignment="center"
+    class="table w-100 position-absolute aggregation-table--top" size="sm">
+    <ng-container *ngFor="let packet of metaInformationValues; trackBy: trackGroupName">
+      <ng-container *ngIf="packet.columns?.length">
+        <tbody width="100%">
+          <tr class="aggregation-table__header">
+            <td align="left" [attr.colspan]="1 + columnConfig.length">
+              {{ packet.packetName }}
+              <span *ngIf="packet.disclaimer"> - <span class="aggregation-table__header--disclaimer">{{
+                  packet.disclaimer }}</span></span>
+            </td>
+          </tr>
+          <ng-container
+            *ngFor="let item of packet.columns | contains:searchValue:'shortHeaderName'; trackBy: trackAggregationField">
+            <tr *ngIf="!item.hideIfEmpty || item.hasDataAvailable" class="aggregation-table__row"
+              [class.aggregation-table__row--selected]="item === selectedDrillDown">
+              <td align="left" width="33%">
+                <ng-container
+                  *ngTemplateOutlet="item.drillDownDefinition ? displayNameAnchorWrapper : displayName; context: { $implicit: item }">
+                </ng-container>
+                <gs-icon *ngIf="item.columnProperties.tooltip" name="info" type="outlined" class="aggregation-info-icon"
+                  [gsTooltip]="tooltipTpl" gsTooltipPlacement="right">
+                </gs-icon>
+
+                <ng-template #tooltipTpl>
+                  <span [innerHtml]="item.columnProperties.tooltip"></span>
+                </ng-template>
+              </td>
+              <ng-container *ngFor="let column of columnConfig; trackBy: trackColumn">
+                <td align="right" [style.width.%]="66 / columnConfig.length" *ngIf="!column.hideFrom360Page">
+                  <ng-template #dynamicCmpHost *ngIf="item.columnProperties.cellRendererFramework">
+                  </ng-template>
+                  <ng-container *ngIf="!item.columnProperties.cellRendererFramework">
+                    <ng-container
+                      *ngTemplateOutlet="standardMetricEntry; context: { $implicit: item, value: currentTabData[item.columnProperties.replacedFieldName.replace('{source}', column.column)], column: column.column, type: column.type, account: selectedAccount?.invest1Id }">
+                    </ng-container>
+                  </ng-container>
+                </td>
+              </ng-container>
+            </tr>
+          </ng-container>
+        </tbody>
+      </ng-container>
+    </ng-container>
+  </gs-table>
+
+  <div *ngIf="error" class="mt-2 px-2">
+    <gs-alert [dismissible]="false" status="error" emphasis="subtle" class="mb-1">
+      An error occurred retrieving data for this account.
+      <gs-button actionType="secondary" emphasis="minimal" class="gs-alert-error__retry-button" (click)="retry()">
+        <gs-icon name="refresh" type="filled">
+        </gs-icon> Try Again
+      </gs-button>
+    </gs-alert>
+  </div>
+</div>
+<ng-template #displayName let-item>{{ item.shortHeaderName }}
+</ng-template>
+<ng-template #displayNameAnchorWrapper let-item>
+  <a (click)="drillDownIntoMetric(item)" class="subtle text-primary">
+    <ng-container *ngTemplateOutlet="displayName; context: { $implicit: item }">
+    </ng-container>
+  </a>
+</ng-template>
+<ng-template #standardMetricEntry let-item let-type="type" let-account="account" let-column="column" let-value="value">
+  <ng-container *ngIf="value !== undefined; else noData">
+    <!-- tidy up column configs to have type -->
+    <ng-container *ngIf="column === 'compliant'">
+      <span [ngClass]="value ? 'aggregation-table__row--compliant' : 'aggregation-table__row--not-compliant'">{{
+        value ? 'Yes' : 'No' }}</span>
+    </ng-container>
+    <!-- tidy up column configs to have type -->
+    <ng-container *ngIf="column === 'target' && type !== 'number'">
+      <ng-container *ngIf="value; else noData">{{ value
+        }}</ng-container>
+    </ng-container>
+    <!-- tidy up column configs to have type -->
+    <ng-container *ngIf="['compliant', 'target'].includes(column) === false || type === 'number'">
+      <ng-container *ngIf="column === 'net'">
+        <span>{{ (item.columnProperties.aggregationType ?? item.columnProperties.type) === 'number' && value >= 0.01 ?
+          '+' : ''
+          }}</span>
+      </ng-container>
+      <span>{{ ((item.columnProperties.aggregationType ?? item.columnProperties.type) === 'number' ? (value |
+        number:'1.2-2') ?? '-' :
+        value) }}</span>
+    </ng-container>
+  </ng-container>
+</ng-template>
+<ng-template #noData>-</ng-template>
